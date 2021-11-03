5bf8c9644b62637e94527a75f91c91c25ed74ce6
kepler4567 <kepler.batista456@gmail.com>
2021-11-03 10:32:16 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/footer';
+import Navbar from 'components/navBar';
 import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Navbar/>
+
+    <div className="container">
        <h1>ola mundo</h1>
+       <DataTable/>
     </div>
+    
+    <Footer/>
+    </>
   );
 }
 
